8e7aec77801bccfd7bfc0b7701bbfe34633d6892
Joshua Homer <countjoshington@gmail.com>
2017-08-20 16:01:52 -0600

    implemented environment specific keys selection

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    cookieKey: process.env.COOKIE_KEY,
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    twitchClientID: process.env.TWITCH_CLIENT_ID,
+    twitchClientSecret: process.env.TWITCH_CLIENT_SECRET,
+    mongodbUri: process.env.MONGO_DB_URI,
+    apiUrl: process.env.API_URL,
+    ravenUrl: process.env.RAVEN_URL
+};
